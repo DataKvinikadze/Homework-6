52a5b0e3902b11c862995ebc197bc010b3400cd1
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-16 00:14:15 +0400

    feature/add Form.jsx structure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <div>
+      <div>
+        <form action="">
+          <div>
+            <h1>Let’s connect constellations</h1>
+            <p>
+              Let's align our constellations! Reach out and let the magic of
+              collaboration illuminate our skies.
+            </p>
+            <div>
+              <div>
+                <input type="text" />
+                <input type="text" />
+              </div>
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+            </div>
+          </div>
+          {/* Image */}
+          <div></div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
