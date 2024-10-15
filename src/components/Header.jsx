d99afe2313afe779424738154ba40b5c26b12337
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-15 23:57:47 +0400

    feature/create basic Header Structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h1>Get in touch</h1>
+      <p>Reach out, and let's create a universe of possibilities together!</p>
+    </div>
+  );
 };
