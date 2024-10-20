1cffba1a2cce7ae11efe3dd174b2df4b5d7f99be
DataKvinikadze <datakvinikadze07@gmail.com>
2024-10-20 22:27:01 +0400

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Header } from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <div className="bg-[#0A0D17]">
+      <div className="bg-[#0A0D17] h-[100vh]">
         <Header />
+        <Form />
       </div>
     </>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,8 +4,8 @@ import FormImage from "../assets/FormImage.svg";
 const Form = () => {
   return (
     <div className="max-w-[1440px] w-full flex justify-center mx-auto">
-      <div className="max-w-[1256px] w-full flex justify-center items-center text-[#FFFFFF] pr-[184px] gap-x-[30px] border-[2px] rounded-[20px] border-[#C38A8A] backdrop:blur-md">
-        <div className="flex flex-col flex-[1] h-[536px] ml-[20px] p-[40px]">
+      <div className="max-w-[1256px] w-full flex justify-center items-center text-[#FFFFFF] pr-[184px] gap-x-[30px] border-[2px] rounded-[20px] bg-[#FFFFFF] bg-opacity-5 backdrop-blur-[380px] py-[20px] border-[#0A0D17] backdrop:blur-md">
+        <div className="flex flex-col flex-[1] ml-[20px] p-[40px]">
           <h1 className="font-semibold text-[30px] text-[#FFFFFF] mb-[8px]">
             Let’s connect constellations
           </h1>
@@ -13,14 +13,35 @@ const Form = () => {
             Let's align our constellations! Reach out and let the magic of
             collaboration illuminate our skies.
           </p>
-          <div className="mt-[41px] flex flex-col gap-y-[14px]">
-            <div>
-              <input type="text" placeholder="Last Name" />
-              <input type="text" placeholder="First Name" />
-            </div>
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Phone Number" />
-            <input type="text" placeholder="Message" />
+          <div className="mt-[41px] grid grid-cols-2 gap-[14px]">
+            <input
+              className="col-start-1 row-start-1 px-[14px] py-[12px] rounded-[5px] bg-[#ffffff] bg-opacity-5 text-[#FFFFFF] border-[#FFFFFF] border-opacity-20 border-[1px]"
+              type="text"
+              placeholder="Last Name"
+            />
+            <input
+              className="col-start-2 row-start-1 px-[14px] py-[12px] rounded-[5px] bg-[#ffffff] bg-opacity-5 text-[#FFFFFF] border-[#FFFFFF] border-opacity-20 border-[1px]"
+              type="text"
+              placeholder="First Name"
+            />
+            <input
+              className="row-start-2 col-span-2 px-[14px] py-[12px] rounded-[5px] bg-[#ffffff] bg-opacity-5 text-[#FFFFFF] border-[#FFFFFF] border-opacity-20 border-[1px]"
+              type="text"
+              placeholder="Email"
+            />
+            <input
+              className="row-start-3 col-span-2 px-[14px] py-[12px] rounded-[5px] bg-[#ffffff] bg-opacity-5 text-[#FFFFFF] border-[#FFFFFF] border-opacity-20 border-[1px]"
+              type="text"
+              placeholder="Phone Number"
+            />
+            <textarea
+              className="row-start-4 col-span-2 h-[109px] px-[14px] py-[12px] rounded-[5px] bg-[#ffffff] bg-opacity-5 text-[#FFFFFF] border-[#FFFFFF] border-opacity-20 border-[1px]"
+              type="text"
+              placeholder="Message"
+            />
+            <button className="col-span-2 w-full bg-gradient-to-r from-[#763AF5] to-[#A604F2] h-[42px] py-[12px] rounded-[5px]">
+              Hello
+            </button>
           </div>
         </div>
         {/* Image */}
